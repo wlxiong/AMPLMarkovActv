b1ea8395344f341ffb6902989f3e431f103c9e29
Yiliang Xiong <wlxiong@gmail.com>
2012-02-25 22:33:18 +0800

    switch b[1] and b[2] axes and plot the estimates

--- plotLL.m
+++ plotLL.m
@@ -1,28 +1,33 @@
 run DATA/LL.m
+run DATA/MLE.m
 whos
 
 figure; hold on
 plot(t, lf_b1)
 plot(b1, ml, '*')
+plot(b1_, ml_, 'o')
 xlabel('b_1')
 ylabel('Likelihood')
 
 figure; hold on
 % The vertices of the mesh lines are the triples (x(j), y(i), Z(i,j)).
 % Note that x corresponds to the columns of Z and y corresponds to the rows.
-mesh(tt, tt, lf_b12)
-plot3(b2,b1,ml, '*')
-
+mesh(tt, tt, lf_b12')
+plot3(b1,b2,ml, '*')
+plot3(b1_,b2_,ml_, 'o')
 xlabel('b_1')
 ylabel('b_2')
 zlabel('Likelihood')
 view(35,30)
 
 figure; hold on
-minc = min(0:-10000:min(lf_b12));
+minc = min(0:-10000:min(lf_b12(:)));
 maxc = max(minc:5000:ml);
-[cs h] = contourf(tt, tt, lf_b12, [minc:20000:ml, maxc]);
+[cs h] = contourf(tt, tt, lf_b12', [minc:20000:ml, maxc]);
 clabel(cs, h, 'FontSize', 14, 'Color', 'r', 'Rotation', 0);
-colormap summer
-text(b2,b1, sprintf('(%.1f,%.1f)', b2, b1))
-plot(b2,b1,'*')
+plot(b1,b2,'*')
+plot(b1_,b2_,'o')
+text(b1,b2, sprintf('(%.1f,%.1f)', b1, b2))
+text(b1_,b2_, sprintf('(%.1f,%.1f)', b1_, b2_))
+xlabel('b_1')
+ylabel('b_2')
