9572ea1d535b4c2df50dabb053f0288fb8ec287e
Yiliang Xiong <wlxiong@gmail.com>
2012-02-26 11:16:38 +0800

    change parameters of activity 1 and 2
    and refine the drawing

--- plotLL.m
+++ plotLL.m
@@ -9,17 +9,23 @@ plot(b1_, ml, '*')
 xlabel('b_1')
 ylabel('Likelihood')
 
-figure; hold on
+figure('Position', [5 5 900 360])
+subplot(1,2,2)
+pbaspect([1 1 1])
+hold on
+box on
 % The vertices of the mesh lines are the triples (x(j), y(i), Z(i,j)).
 % Note that x corresponds to the columns of Z and y corresponds to the rows.
-mesh(tt, tt, lf_b12')
+surf(tt, tt, lf_b12', 'FaceAlpha', .9);
 plot3(b1_,b2_,ml, '*')
 xlabel('b_1')
 ylabel('b_2')
 zlabel('Likelihood')
-view(35,30)
+view(40,35)
 
-figure; hold on
+subplot(1,2,1)
+pbaspect([1 1 1])
+hold on
 minc = min(0:-10000:min(lf_b12(:)));
 maxc = max(minc:5000:ml);
 [cs h] = contourf(tt, tt, lf_b12', [minc:20000:ml, maxc]);
