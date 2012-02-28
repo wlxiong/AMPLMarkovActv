dd904e923af3684723cac7277a3b3e10f33c29a4
Yiliang Xiong <wlxiong@gmail.com>
2012-02-28 17:32:42 +0800

    clean up the Monte Carlo simulation codes

--- runMC.m
+++ runMC.m
@@ -17,30 +17,32 @@ travelTime = [
 
 H = 288;	% number of time slices, and T * H = 1440 minutes
 N = 300;	% number of individuals
+HOME = 1;	% index of HOME activity
 
 xt = zeros(N,H,'int8');	% travelers' choices
 dt = zeros(N,H,'int8');	% travelers' states
 
 for n = 1:N
-	t = 1;
 	fprintf('\t%3d', n)
-	xt(n,1) = 1;		% we require that the dividual stay at home in time slice 1
+	xt(n,1) = HOME;		% the dividual stay at home in time slice 1
+	t = 1;
 	while t <= H
         p = Pr(t,xt(n,t),:);
         p = p(:)./sum(p);      % make sure the probabilites sum to one
 		dt(n,t) = find( mnrnd(1, p') );
-		xt(n, t + travelTime(xt(n,t), dt(n,t)) + 1 ) = dt(n,t);
 		for s = t + 1 : t+travelTime(xt(n,t), dt(n,t))
 			xt(n,s) = 0;
 			dt(n,s) = 0;
 		end
+		% state transition
+		xt(n, t + travelTime(xt(n,t), dt(n,t)) + 1 ) = dt(n,t);
 		t = t + travelTime(xt(n,t),dt(n,t)) + 1;
 	end
 end
 
 % save the simulated data
 fprintf('\n save the simulation data')
-save 'DATA/MC.mat' xt dt H N
+save 'DATA/MC.mat' xt dt EV Pr H N
 
 % export the data to an AMPL .dat file
 fprintf('\n export the data as .dat\n')
