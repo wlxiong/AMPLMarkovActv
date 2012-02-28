f4ebf37a9ea3ae0208f5155d594ec5c64c96df6c
Yiliang Xiong <wlxiong@gmail.com>
2012-02-28 17:33:09 +0800

    visualize the simulation data

---
+++ viewMC.m
@@ -0,0 +1,35 @@
+clear
+load 'DATA/MC.mat' EV xt H N
+whos
+
+tt = [1:H]';
+
+% visualize EV and Pr
+figure
+plot(tt/H, EV(1:H,:))
+set(gca, 'XTick', .0:1.0/6.0:1.0);
+datetick('x', 'HH:MM', 'keepticks');
+
+% visualize xt
+figure
+hold on
+for n = 1:N
+	xx = xt(n,1:H);
+	ii = [xx(:) ~= 0];
+	xx = xx(ii);
+	plot(tt(ii)/H, xx)
+end
+ylim([.5 3.5])
+set(gca, 'XTick', .0:1.0/6.0:1.0);
+datetick('x', 'HH:MM', 'keepticks');
+
+% caculate number of trips
+num_trips = zeros(1,N);
+for n = 1:N
+	xx = xt(n,1:H);
+	edges = diff([0;xx(:) == 0]);
+	num_trips(n) = sum(edges > 0);
+end
+% draw histgram of trip numbers
+figure
+hist(num_trips)
