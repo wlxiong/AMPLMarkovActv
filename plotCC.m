6c96f91d855c7ac0101b3413fffb6b9ebda31271
Yiliang Xiong <wlxiong@gmail.com>
2012-03-01 22:31:40 +0800

    plot Cauchy curves defined by true values and estimates

---
+++ plotCC.m
@@ -0,0 +1,26 @@
+function plotCauchyCurve(Um, b, c, filename)
+% plot marginal utility function
+
+figure
+hold on
+grid on
+box off
+
+tt = 0:30:1440;
+pdf = @(u0, b0, c0) u0./pi./c0./( ( (tt-b0)/c0 ).^2 + 1 );
+lsty = {'-ro', '-r*', '-r^', '-rs'};
+for i = 1:length(Um)
+	plot(tt/1440.0, pdf(Um(i), b(i), c(i)), lsty{i}, 'LineWidth', 1)
+end
+
+set(gca, 'XTick', .0:1.0/6.0:1.0);
+datetick('x', 'HH:MM', 'keepticks');
+pbaspect([2 1 1])
+xlabel('Time of the day')
+ylabel('Marginal utility')
+
+axis([.0 1.0 0.0 6])
+pbaspect([2 1 1])
+legend({'Home-AM', 'Work', 'Shopping', 'Home-PM'},'Location','NorthWest')
+legend('boxoff')
+export_fig(['FIGURES/' filename], '-pdf')
--- plotMU.m
+++ plotMU.m
@@ -3,36 +3,19 @@ function plotMU
 
 clear all
 
-% load the parameters
+% load the true values and estimates
 run DATA/MDP.m
+run DATA/MLE.m
+whos
 
 % HOME-PM
-Um(end+1) = Um(1);
-b(end+1) = b(1)+1440;
-c(end+1) = c(1);
+Um0(end+1) = Um0(1);
+b0(end+1) = b0(1)+1440;
+c0(end+1) = c0(1);
+Um_(end+1) = Um_(1);
+b_(end+1) = b_(1)+1440;
+c_(end+1) = c_(1);
 
-figure
-hold on
-grid on
-box off
-
-% marginal utility function
-tt = 0:30:1440;
-pdf = @(u_, b_, c_) u_./pi./c_./( ( (tt-b_)/c_ ).^2 + 1 );
-
-lsty = {'-ro', '-r*', '-r^', '-rs'};
-for i = 1:length(Um)
-	plot(tt/1440.0, pdf(Um(i), b(i), c(i)), lsty{i}, 'LineWidth', 1)
-end
-
-set(gca, 'XTick', .0:1.0/6.0:1.0);
-datetick('x', 'HH:MM', 'keepticks');
-pbaspect([2 1 1])
-xlabel('Time of the day')
-ylabel('Marginal utility')
-
-axis([.0 1.0 0.0 6])
-pbaspect([2 1 1])
-legend({'Home-AM', 'Work', 'Shopping', 'Home-PM'},'Location','NorthWest')
-legend('boxoff')
-export_fig('FIGURES/cauchy_util', '-pdf')
+% plot the true and the estimated  marginal utility function
+plotCC(Um0, b0, c0, 'cauchy_util_0')
+plotCC(Um_, b_, c_, 'cauchy_util_e')
