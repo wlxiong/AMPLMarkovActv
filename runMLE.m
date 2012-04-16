09ae51ed8e08a712d80aae9dd6cb780598dd4390
Yiliang Xiong <wlxiong@gmail.com>
2012-04-16 14:39:29 +0800

    clean up codes and some minor changes

--- runMLE.m
+++ runMLE.m
@@ -1,13 +1,11 @@
-function [LLvec VoTvec Uvec Bvec Cvec] = runMLE
-% bootstrap
+function [LLvec VOTvec Uvec Bvec Cvec] = runMLE(B,A)
+% B: number of trials in bootstrap
+% A: number of activities
+% bootstrap MLE
 
 % set random seed
 rand_seed = 31415196;
 rng(31415196);
-% number of trials in bootstrap
-B = 50;
-% number of activities
-A = 3;
 % initialize estimates
 Uvec = zeros(B,A);
 Bvec = zeros(B,A);
@@ -16,7 +14,7 @@ LLvec = zeros(B,1);
 VOTvec = zeros(B,1);
 
 for j = 1:B
-	% run MC simulation
+	% run simulation
 	fprintf('\n run simulation (%d/%d)\n', j, B)
 	runMC
 	% call AMPL to estimate the parameters
@@ -32,4 +30,4 @@ for j = 1:B
 end
 
 % save the estimates
-save  DATA/MLE LLvec VoTvec Uvec Bvec Cvec
+save  DATA/MLE LLvec VOTvec Uvec Bvec Cvec
