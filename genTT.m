ff2a8fde3cabd41c9b66fe654c87f6af9a7f6641
Yiliang Xiong <wlxiong@gmail.com>
2013-02-16 15:33:43 +0800

    create a standard MDP branch
    1) standard MDP only allows primitive choices with unit duration
    2) travel time is set to 1x constant
    3) results similar to MDP with temporal decisions are obtained (see DATA\EUtil.m)

--- genTT.m
+++ genTT.m
@@ -44,7 +44,7 @@ t = 0:5:1439;
 pdf = @(m_, b_, c_) m_./( ( (t-b_)./c_ ).^2 + 1 );
 
 % BPR function
-bpr = @(x, k, t0) t0 .* ( 1.0 + (x./k).^4);
+bpr = @(x, k, t0) t0;
 
 whos
 figure
@@ -73,7 +73,7 @@ ylabel('Travel time (\times 5 min)')
 axis([.0 1.0 0.0 12])
 pbaspect([2 1 1])
 legend('boxoff')
-export_fig('FIGURES/travel_time', '-pdf')
+% export_fig('FIGURES/travel_time', '-pdf')
 
 fprintf('save travel time\n')
 save 'DATA/TT.mat' travelTime
