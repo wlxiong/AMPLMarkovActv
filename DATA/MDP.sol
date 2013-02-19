665a4882b12c4b851d3a7dcee878dd52cacce302
Yiliang Xiong <wlxiong@gmail.com>
2013-02-19 19:12:04 +0800

    import the current data and figures

---
+++ DATA/MDP.sol
@@ -0,0 +1,36 @@
+Component MDP 1
+Optimal solution found
+    0 
+_solve_time = 1.51321
+
+beta = 0.95
+VoT = 60
+theta = 1
+
+:      Um     b      c     :=
+1 1   3600      0   320
+1 2   2500    840   180
+1 3   1200   1140   210
+2 1   3600      0   320
+2 2   2500    840   180
+2 3   1200   1140   210
+;
+
+Component MDP 2
+Optimal solution found
+    0 
+_solve_time = 1.48201
+
+beta = 0.95
+VoT = 60
+theta = 1
+
+:      Um     b      c     :=
+1 1   3600      0   320
+1 2   2500    840   180
+1 3   1200   1140   210
+2 1   3600      0   320
+2 2   2500    840   180
+2 3   1200   1140   210
+;
+
---
+++ DATA/jointMDP.sol
@@ -0,0 +1,18 @@
+Composite MDP
+Optimal solution found
+    0 
+_solve_time = 0.483603
+
+beta = 0.95
+VoT = 60
+theta = 1
+
+:      Um     b      c     :=
+1 1   3600      0   320
+1 2   2500    840   180
+1 3   1200   1140   210
+2 1   3600      0   320
+2 2   2500    840   180
+2 3   1200   1140   210
+;
+
