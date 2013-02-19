5d758f561039b64aef4d365f5b9a8effce826e65
Yiliang Xiong <wlxiong@gmail.com>
2013-02-20 00:51:47 +0800

    compare the time use of the two individual within a household

--- testFXX.m
+++ testFXX.m
@@ -4,16 +4,16 @@
 % plotFX(squeeze(fx(1,:,:)))
 
 clear functions
-run('DATA\FXX00.m')
+run('DATA\FXX.m')
 figure; grid off; box off
-fx1 = squeeze(fxx(1,:,:));
+fx1 = squeeze(fx(1,:,:));
 plotFX(fx1)
 
 clear functions
-run('DATA\FXX02.m')
+run('DATA\FXX.m')
 figure; grid off; box off
-fx2 = squeeze(fxx(1,:,:));
+fx2 = squeeze(fx(2,:,:));
 plotFX(fx2)
 
 figure; grid off; box off
-ah = plotAH(fx1, fx2);
+ah = plotAH(fx1, fx2, {'HH member 1', 'HH member 2'});
