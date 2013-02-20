1218b31cadb51bd35dafa911b30f3798c8959a26
Yiliang Xiong <wlxiong@gmail.com>
2013-02-20 16:18:37 +0800

    change the marker for plot

--- plotUA.m
+++ plotUA.m
@@ -4,9 +4,10 @@ T = 15;
 H = 1439;
 x = (0:T:H) / 1440.0;
 hold on
-plot(x', utils(1, 1:(T/5):288), '-ko', 'LineWidth', 1, 'MarkerSize', 6)
-plot(x', utils(2, 1:(T/5):288), '-ks', 'LineWidth', 1, 'MarkerSize', 6)
-plot(x', utils(3, 1:(T/5):288), '-k*', 'LineWidth', 1, 'MarkerSize', 6)
+plot(x, utils(1, 1:(T/5):288), '-ko', 'LineWidth', 1, 'MarkerSize', 6)
+plot(x, utils(2, 1:(T/5):288), '-kd', 'LineWidth', 1, 'MarkerSize', 6)
+plot(x, utils(3, 1:(T/5):288), '-k^', 'LineWidth', 1, 'MarkerSize', 6)
+% plot(x, max(utils(:,1:(T/5):288), [], 1), '-k', 'LineWidth', 2)
 
 pbaspect([2 1 1])
 set(gca, 'XTick', .0:1.0/6.0:1.0);
